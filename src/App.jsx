38b01fea70d8c519ca3a5e1c42eef0a468101460
Gajanan06 <gajanannilajkar06@gmail.com>
2026-04-28 12:04:51 +0530

    Added basic routing and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
-
   return (
-    <>
-      <h1>Product Catagory App</h1>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div style={{
+      padding: "16px",
+      borderBottom: "1px solid #ddd",
+      fontSize: "20px",
+      fontWeight: "bold"
+    }}>
+      Product Catalog
+    </div>
+  );
+};
+
+export default Header;
