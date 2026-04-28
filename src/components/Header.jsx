2435d1487d05585630bdb54c3cbb056d8f7ee3f8
Gajanan06 <gajanannilajkar06@gmail.com>
2026-04-28 12:25:27 +0530

    Explored data and added data on UI with basic styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,8 @@
-const Header = () => {
-  return (
-    <div style={{
-      padding: "16px",
-      borderBottom: "1px solid #ddd",
-      fontSize: "20px",
-      fontWeight: "bold"
-    }}>
-      Product Catalog
-    </div>
-  );
-};
+
+const Header = () => (
+  <div className="header">
+    Product Catalog
+  </div>
+);
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import data from "../data/data.json";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const groupedData = {};
+
+  data.forEach((item) => {
+    if (!groupedData[item.category]) {
+      groupedData[item.category] = [];
+    }
+    groupedData[item.category].push(item);
+  });
+  console.log(groupedData);
+
+  return (
+    <div className="container">
+      {Object.keys(groupedData).map((category) => (
+        <div key={category} className="category">
+          <h3>{category}</h3>
+
+          <div className="grid">
+            {groupedData[category].map((item) => (
+              <div key={item.id} className="card">
+                <img src={item.image} alt={item.itemname} />
+                <p>{item.itemname}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
